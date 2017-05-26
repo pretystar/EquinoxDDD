3a0be347d097a66407b8254094b03cbed4aa2f6e
pretystar <cocoler@yeah.net>
2017-05-26 17:03:57 +0800

    my

--- src/Equinox.Webapi/Program.cs
+++ src/Equinox.Webapi/Program.cs
@@ -17,7 +17,6 @@ namespace Equinox.Webapi
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
                 .Build();
 
             host.Run();
